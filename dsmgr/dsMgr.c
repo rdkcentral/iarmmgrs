53a38a6ffef2c39f8a98ebc270b6bdb1b76565a8
rababu2 <rababu2@synamedia.com>
2025-03-27 20:47:52 +0530

    Rdk 55443 : DSMgr changes for Power Manager Improvement (#47)
    * dsMgr Changes with connect, retry and power controller cb changes
    
    * dsMgr build errors fix and changes
    
    * systimermgr bbappend  err fix
    
    * iarmmgr-dsMgr service file changes by adding wpeframework-powermanager.service
    
    * Changes with respect to event handling taking more time during processing
    
    * dsmgr component updated....

--- dsmgr/dsMgr.c
+++ dsmgr/dsMgr.c
@@ -162,6 +162,9 @@ IARM_Result_t DSMgr_Start()
 	IARM_Bus_RegisterCall(IARM_BUS_COMMON_API_SysModeChange, _SysModeChange);
 
         /*Refactored dsMGR code*/
+       PowerController_Init();
+       dsMgrInitPwrControllerEvt();
+       /* Power controller connect is checked inside initPwrEventListner*/
         initPwrEventListner();   
 	/* Create  Thread for listening Hot Plug events */
 	pthread_mutex_init (&tdsMutexLock, NULL);
@@ -234,7 +237,8 @@ IARM_Result_t DSMgr_Stop()
     { 
         g_main_loop_quit(dsMgr_Gloop);
     }
-    
+    dsMgrDeinitPwrControllerEvt();
+    PowerController_Term();
     IARM_Bus_Disconnect();
     IARM_Bus_Term();
 	pthread_mutex_destroy (&tdsMutexLock);
