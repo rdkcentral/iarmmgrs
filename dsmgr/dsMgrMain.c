70627936e43e3c2aff9e5dbee1eb5d14d14a6d80
balav08 <54432605+balav08@users.noreply.github.com>
2026-08-12 16:49:37 +0530

    RDKEMW-22348 : Update dsMgrMain.c

--- dsmgr/dsMgrMain.c
+++ dsmgr/dsMgrMain.c
@@ -147,23 +147,8 @@ int main(int argc, char *argv[])
     // write pidfile because sd_notify() does not work inside container
     IARM_Bus_WritePIDFile(xstr(PID_FILE_PATH) "/dsmgr.pid");
 #endif
-    //coverity fix: UNCAUGHT_EXCEPT - wrap in try-catch to handle potential exceptions
-#ifdef __cplusplus
-    try {
-#endif
-        DSMgr_Loop();
-        DSMgr_Stop();
-#ifdef __cplusplus
-    }
-    catch (const std::exception& e) {
-        INT_ERROR("Exception caught in main: %s\n", e.what());
-        return -1;
-    }
-    catch (...) {
-        INT_ERROR("Unknown exception caught in main\n");
-        return -1;
-    }
-#endif
+    DSMgr_Loop();
+    DSMgr_Stop();
     return 0;
 }
 
